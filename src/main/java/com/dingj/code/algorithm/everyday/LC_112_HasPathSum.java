4a35784a009265bf819f8c74f6e1628eaf4d8654
zhiyuanbest <dingjianylx@163.com>
2020-07-07 21:42:12 +0800

    7.7

---
+++ src/main/java/com/dingj/code/algorithm/everyday/LC_112_HasPathSum.java
@@ -0,0 +1,26 @@
+package com.dingj.code.algorithm.everyday;
+
+import com.dingj.code.algorithm.tree.TreeNode;
+
+/**
+ * 112. 路径总和
+ * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+ */
+public class LC_112_HasPathSum {
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+
+        if (root == null) {
+            return false;
+        }
+
+        // 达到叶子节点时，递归终止，判断sum是否符合条件
+        if (root.left == null && root.right == null) {
+            return root.val == sum;
+        }
+
+        // 递归地判断root节点的左孩子和右孩子。
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+
+    }
+}
